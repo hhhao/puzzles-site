7f1a39e7240d422a58cb71c5fbe062f5796aae17
Howard Hao <hhhao22@gmail.com>
2017-02-24 19:17:05 -0800

    Fix minimax, NNWeights backup

--- chess_codes/chessMinimax.js
+++ chess_codes/chessMinimax.js
@@ -7,10 +7,11 @@ var chess = new (require('./chessGame.js'))();
 
 //TODO implement tree reordering
 function minimaxTopLevelWithMove(fen, depth) {
-    var result = [-Infinity, null];
+    chess.fenToBoard(fen);
+    var isTopMaxSide = chess.current_move_side === 'w' ? true : false;
+    var result = [(isTopMaxSide ? -Infinity : Infinity), null];
     var alpha = -Infinity;
     var beta = Infinity;
-    chess.fenToBoard(fen);
     var moves = chess.availableMoves();
     if (depth <= 0 || !moves.length) {
         result[0] = nn.forward(chess.gfeatures(), chess.pfeatures(), chess.sfeatures());
@@ -19,11 +20,15 @@ function minimaxTopLevelWithMove(fen, depth) {
     for (let i = 0, n = moves.length; i < n; i++) {
         var m = moves[i];
         chess.move(m[0], m[1], m[2]);
-        var subScore = minimax(chess.boardToFen(), depth-1, alpha, beta, false);
-        if (subScore > result[0]) {
+        var subScore = minimax(chess.boardToFen(), depth-1, alpha, beta, !isTopMaxSide);
+        if (isTopMaxSide && subScore > result[0]) {
             result[0] = subScore;
             result[1] = m;
             alpha = subScore;
+        } else if (!isTopMaxSide && subScore < result[0]) {
+            result[0] = subScore;
+            result[1] = m;
+            beta = subScore;
         }
         chess.fenToBoard(fen);
     }
