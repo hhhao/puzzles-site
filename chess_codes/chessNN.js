5255f0a6ea2688d79f6df27f790e34a3a1511bda
Howard Hao <hhhao22@gmail.com>
2017-02-24 23:46:33 -0800

    Delete debug message, change NN save file name

--- chess_codes/chessNN.js
+++ chess_codes/chessNN.js
@@ -110,7 +110,6 @@ ChessNN.prototype = {
     //back propagation
     backprop: function(error) {
         var dout = error; //L2 loss, for L1 use error < 0 ? -1 : (error > 0 ? 1 : 0);
-        console.log(this.h2s);
         var dh2s = dout * this.dtanh(this.h2s[0][0]);
         var dh2w = math.multiply(dh2s, math.transpose(this.h2));
         var dh2b = [[dh2s]];
@@ -242,7 +241,7 @@ ChessNN.prototype = {
                       };
 
         console.log('write weights, gb: ', dataObj.gb);
-        fs.writeFileSync(__dirname + "/NNWeights_test.json", JSON.stringify(dataObj));
+        fs.writeFileSync(__dirname + "/NNWeights.json", JSON.stringify(dataObj));
 
     }
 };
